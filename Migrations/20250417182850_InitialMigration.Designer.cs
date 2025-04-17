5b6f5f4b8fd3469e2a9e88e4653ee29eee7c981d
Luis <iluisflores@yahoo.com>
2025-04-17 13:31:41 -0500

    Created SeedUser file to populate database

--- Migrations/20250417182850_InitialMigration.Designer.cs
+++ Migrations/20250417182850_InitialMigration.Designer.cs
@@ -4,6 +4,7 @@ using DrivingEd_BackEnd.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DrivingEd_BackEnd.Migrations
 {
     [DbContext(typeof(DrivingEdDbContext))]
-    partial class DrivingEdDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250417182850_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
